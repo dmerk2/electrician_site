444303266242bcc31fa8c57b85a62bc6cb8a6c80
Daniel Merkin <danmerkin@Dans-MBP.home>
2017-03-04 05:05:05 -0500

    made clickToCall links

--- html/process-contact.php
+++ html/process-contact.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "dan.merkin@gmail.com";  // Your email here
+    $to = "htdevs@gmail.com";  // Your email here
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
 	$phone = $_REQUEST['phone'];
